c4f1f13f1b43d3ab1bc83c0570df583863e5ef72
Mondrell <mondrellharrison@yahoo.com>
2025-09-25 03:28:14 -0500

    Fix settings to ignore extra env vars

--- app/core/config.py
+++ app/core/config.py
@@ -1,10 +1,16 @@
 # app/core/config.py
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
-    DATABASE_URL: str  # add this line for Render to pick up
-    
-    class Config:
-        env_file = ".env"
+    DATABASE_URL: str
+    SECRET_KEY: str
 
-settings = Settings() 
+    model_config = SettingsConfigDict(
+        env_file=".env",   # load from .env
+        extra="ignore"     # <-- accept extra env vars
+    )
+
+
+# Create the settings instance
+settings = Settings()  # type: ignore
